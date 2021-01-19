d9293cae48883758a62a361197268b48e9a600cd
Dheeraj Garikapati <garikapati.dheeraj@gmail.com>
2021-01-19 17:08:26 +0530

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to contact_book
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/contact_book/core_test.clj
@@ -0,0 +1,7 @@
+(ns contact-book.core-test
+  (:require [clojure.test :refer :all]
+            [contact-book.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
